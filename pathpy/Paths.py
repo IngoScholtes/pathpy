45d43269baa0f0a11ea1cfc918af288e41e1a0fa
ischoltes@ethz.ch <ischoltes@ethz.ch>
2017-03-02 15:44:46 +0100

    fixed semantics of addPathTuple

--- pathpy/Paths.py
+++ pathpy/Paths.py
@@ -517,7 +517,7 @@ class Paths:
                     contained = Paths.getContainedPaths(x, node_filter)                    
                     for s in contained:
                         if len(s)-1>=minLength and len(s)-1<=maxLength:
-                            p.addPathTuple(s, expandSubPaths=True, frequency=self.paths[l][x][1])
+                            p.addPathTuple(s, expandSubPaths=True, frequency=(0, self.paths[l][x][1]))
         return p
 
 
@@ -539,7 +539,7 @@ class Paths:
                     newP = ()
                     for v in x:
                         newP += (mapping[v],)
-                    p.addPathTuple(newP, expandSubPaths=True, frequency=self.paths[l][x][1])
+                    p.addPathTuple(newP, expandSubPaths=True, frequency=(0, self.paths[l][x][1]))
         return p
 
 
@@ -557,7 +557,7 @@ class Paths:
             for the trigram a;b;c a path a->b->c of length two will be generated 
             as well as two subpaths a->b and b->c of length one
 
-        @weight weight: the weight (i.e. frequency) of the ngram
+        @weight pathFrequency: the number of occurrences (i.e. frequency) of the ngram
         """
 
         fields = ngram.rstrip().split(separator)
